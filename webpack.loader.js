be743367d40351348e78363be287f140c8ac61a7
xwjune <xwjune@163.com>
2020-05-30 19:09:53 +0800

    update postcss

--- webpack.loader.js
+++ webpack.loader.js
@@ -1,5 +1,4 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const autoprefixer = require('autoprefixer');
 
 const loaders = [
   {
@@ -24,23 +23,7 @@ const loaders = [
     use: [
       MiniCssExtractPlugin.loader,
       'css-loader',
-      {
-        loader: 'postcss-loader',
-        options: {
-          plugins: [
-            autoprefixer({
-              browsers: [
-                'last 5 versions',
-                'last 5 Chrome versions',
-                'Firefox >= 17',
-                'ie >= 8',
-                'iOS >= 8',
-                'Android >= 4',
-              ],
-            }),
-          ]
-        },
-      },
+      'postcss-loader',
       'sass-loader',
     ],
   },
